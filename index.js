35812879ffd86ba16a5da091ef1a788c15ab8123
Vicente Vega Ulloa <vnvega@uc.cl>
2022-03-04 12:47:55 -0300

    fix cors

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ db.sequelize
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
